79ec85c8314835d71b19e2d953efb5c2a5774e28
AF <a.harjunpaa02@gmail.com>
2023-10-11 12:20:09 +0300

    Restore all

---
+++ Assets/Scripts/FlameThrower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrower : MonoBehaviour
+{
+    public GameObject fireBallPrefab;
+    public Transform firePoint;
+    public float firePower = 5f;
+    public float fireRate = 0.5f;
+    private float nextFireTime = 0.0f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time >= nextFireTime)
+        {
+            GameObject newFireBall = Instantiate(fireBallPrefab, firePoint.position, firePoint.rotation);
+
+            float randomSize = Random.Range(0.25f, 0.75f);
+            newFireBall.transform.localScale = new Vector3(randomSize, randomSize, randomSize);
+
+            Rigidbody2D fireBallRB = newFireBall.GetComponent<Rigidbody2D>();
+            fireBallRB.AddForce(firePoint.right * firePower, ForceMode2D.Impulse);
+            fireBallRB.AddForce(firePoint.up * Random.Range(-3f, 3f), ForceMode2D.Impulse);
+
+            Destroy(newFireBall, 0.5f);
+
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+}

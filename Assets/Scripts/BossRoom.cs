78e138a749fc36c7ad5b862e3d72ab05cae752aa
AF <a.harjunpaa02@gmail.com>
2023-10-12 22:40:11 +0300

    bug fixes

---
+++ Assets/Scripts/BossRoom.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoom : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject bossPrefab;
+    public Transform bossSpawn;
+
+    void OnEnable()
+    {
+        door.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            door.SetActive(true);
+            Instantiate(bossPrefab, bossSpawn.position, Quaternion.identity);
+        }
+    }
+}
